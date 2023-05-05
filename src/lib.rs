6589ca3700e91686f80628fbb24441c444711fbc
megahomyak <g.megahomyak@gmail.com>
2023-05-05 12:27:54 +0500

    Made tests better a little

--- src/lib.rs
+++ src/lib.rs
@@ -174,6 +174,7 @@ mod tests {
                 .block_on(
                     async move { tokio::join!(dummy_handler(waiter_1), dummy_handler(waiter_2)) },
                 );
+            assert!(dispatcher.contexts.lock().unwrap().is_empty());
             assert_eq!(result_1, HashSet::from(["1: 1", "1: 2", "1: 3"]));
             assert_eq!(result_2, HashSet::from(["2: 1", "2: 2", "2: 3"]));
         }
